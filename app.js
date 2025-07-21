207d5223d29b87aa268b59081e1be10eddd9adcb
Fareeha Abbasi <abbasifareeha9@gmail.com>
2025-07-21 13:18:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -227,30 +227,30 @@ loginBtn &&
 
 
 //--------------- ADD A POST ------------>
-const submitBtn = document.getElementById("post-btn");
-const loader = document.getElementById("loader-overlay");
+// const submitBtn = document.getElementById("post-btn");
+// const loader = document.getElementById("loader-overlay");
 
-function showLoader() {
-    loader.style.display = 'flex';
-}
+// function showLoader() {
+//     loader.style.display = 'flex';
+// }
 
-function hideLoader() {
-    loader.style.display = 'none';
-};
-submitBtn &&
-    submitBtn.addEventListener('click', async () => {
-        const postTitle = document.getElementById('title').value.trim();
-        const postDes = document.getElementById('description').value.trim();
+// function hideLoader() {
+//     loader.style.display = 'none';
+// };
+// submitBtn &&
+//     submitBtn.addEventListener('click', async () => {
+//         const postTitle = document.getElementById('title').value.trim();
+//         const postDes = document.getElementById('description').value.trim();
 
-        if (!postTitle || !postDes) {
-            Swal.fire({
-				icon: 'warning',
-				title: 'Missing Fields',
-				text: 'Please enter both a title and a description.',
-				confirmButtonColor: '#125b9a',
-			});
-			return;
-      }
-      showLoader();
-		submitPost.disabled = true;
-    })
+//         if (!postTitle || !postDes) {
+//             Swal.fire({
+// 				icon: 'warning',
+// 				title: 'Missing Fields',
+// 				text: 'Please enter both a title and a description.',
+// 				confirmButtonColor: '#125b9a',
+// 			});
+// 			return;
+//       }
+//       showLoader();
+// 		submitPost.disabled = true;
+//     })
